e98969406f314ed3b1ebb34411daffde9c72d256
tejas-gulhane <103639207+tejas-gulhane@users.noreply.github.com>
2022-07-30 11:15:29 +0530

    expence tracker

---
+++ App.js
@@ -0,0 +1,9 @@
+import './style.css';
+import Expence from './Expence Tracker/expenxe'
+function App() {
+  return (
+    <Expence />
+  );
+}
+
+export default App;
---
+++ Expence Tracker/expenxe.js
@@ -0,0 +1,16 @@
+import '../style.css';
+
+function expence(){
+    return (
+        <div className="App">
+     <h1>Expence Item</h1>
+
+     <p>Food Rs. 10</p>
+     <p>Petrol Rs. 100</p>
+     <p>Movie Rs. 1000</p>
+ 
+    </div>
+    );
+
+}
+export default expence;
